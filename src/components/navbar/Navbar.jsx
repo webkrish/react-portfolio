fff9e4cada4298bb1328a5e913568d2ecf00dd16
Krishnendu Tripathy <krishnendutripathy337@gmail.com>
2024-02-25 15:02:55 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,11 +13,11 @@ function Navbar() {
                     transition={{ duration: 0.5 }}
                     className='logo'>Portfolio</motion.span>
                 <div className="social">
-                    <a href="https://www.facebook.com/profile.php?id=100005249479261"><img src='social-facebook.png' alt="" /></a>
-                    <a href="https://www.instagram.com/krishnendutripathy/"><img src='social-instagram.png' alt="" /></a>
-                    <a href="https://wa.link/4e1hzf"><img src='social-tik-tok.png' alt="" /></a>
-                    <a href="https://www.linkedin.com/in/krishnendu-tripathy-641aa6226/"><img src='social-youtube.png' alt="" /></a>
-                    <a href="https://github.com/webkrish"><img src='git-hub.png' alt="" /></a>
+                    <a target="_blank" href="https://www.facebook.com/profile.php?id=100005249479261"><img src='social-facebook.png' alt="" /></a>
+                    <a target="_blank" href="https://www.instagram.com/krishnendutripathy/"><img src='social-instagram.png' alt="" /></a>
+                    <a target="_blank" href="https://wa.link/4e1hzf"><img src='social-tik-tok.png' alt="" /></a>
+                    <a target="_blank" href="https://www.linkedin.com/in/krishnendu-tripathy-641aa6226/"><img src='social-youtube.png' alt="" /></a>
+                    <a target="_blank" href="https://github.com/webkrish"><img src='git-hub.png' alt="" /></a>
                 </div>
             </div>
         </div>
