96b96424c4a45d8c0cc964e1d05b30640ce8499c
Krishnendu Tripathy <krishnendutripathy337@gmail.com>
2024-02-29 12:18:56 +0530

    Update ToggleButton.jsx

--- src/components/sidebar/ToggleButton/ToggleButton.jsx
+++ src/components/sidebar/ToggleButton/ToggleButton.jsx
@@ -5,16 +5,20 @@ function ToggleButton({setOpen}) {
   return  <button onClick={()=> setOpen((prev) =>! prev)}>
     <svg width='23' height='23'viewBox='0 0 23 23'>
       <motion.path strokeWidth="3" stroke='black' strokeLinecap='round' variants={{
-        closed:{d:"M 2 5 L 20 5"},
-        open:{d:"M 3 16.5 L 17 2.5"},
+        closed:{d:"M 2 5 L 20 5",
+      opacity:1},
+        open:{d:"M 3 16.5 L 17 2.5",
+      opacity:1},
       }}/>
       <motion.path strokeWidth="3" stroke='black' strokeLinecap='round' d="M 2 11.5 L 20 11.5" variants={{
         closed:{opacity:1},
         open:{opacity:0}
       }}/>
       <motion.path strokeWidth="3" stroke='black' strokeLinecap='round' variants={{
-        closed:{d:"M 2 18 L 20 18"},
-        open:{d:"M 3 2.5 L 17 16.5 "},
+        closed:{d:"M 2 18 L 20 18",
+      opacity:1},
+        open:{d:"M 3 2.5 L 17 16.5 ",
+        opacity:1},
       }}/>
       </svg>
   </button>
